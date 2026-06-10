dac8b95bc583059ad3ce8a8994079d99dd6ec69a
Manu04Hubb <emanuelodhiambo84@gmail.com>
2026-06-10 12:54:01 +0300

    charAt O(1) prefixsum basics Stringbuilder and toString

---
+++ src/dev/manu/dsa/ArraysAndStrings/PrefixSum.java
@@ -0,0 +1,39 @@
+package dev.manu.dsa.ArraysAndStrings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrefixSum {
+    static void main() {
+        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6};
+        PrefixSum ps = new PrefixSum(arr);
+        System.out.println(ps.rangeSum(2, 5));
+        System.out.println(ps.rangeSum(0, 3));
+    }
+    private int[] prefix;
+    //O(n) constructor
+    public PrefixSum(int[] arr) {
+        int n = arr.length;
+        prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
+    }
+    //O(1) query
+    public int rangeSum(int low, int high) {
+        return prefix[high + 1] + prefix[low];
+    }
+
+    //advanced sub-array sum
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(0, 1);
+        int prefix = 0, result = 0;
+        for (int num : nums) {
+            prefix += num;
+            result += count.getOrDefault(prefix - k, 0);
+            count.merge(prefix, 1, Integer::sum);
+        }
+        return result;
+    }
+}
--- src/dev/manu/dsa/ArraysAndStrings/StringOperations.java
+++ src/dev/manu/dsa/ArraysAndStrings/StringOperations.java
@@ -1,10 +1,15 @@
 package dev.manu.dsa.ArraysAndStrings;
 
+import java.util.Scanner;
+
 public class StringOperations {
     static void main() {
-        System.out.println(MyStingBuilders());
+        System.out.println(MyStringBuilders());
+        String[] langs1 = {"Java","GO","Rust","Python","C#","TypeScript","JavaScript"};
+        System.out.println("The joined words are " + joinStrings(langs1));
+        System.out.println("Is it a palidrome " + checkIfPalindrome("Silent"));
     }
-    public static String MyStingBuilders() {
+    public static String MyStringBuilders() {
         StringBuilder sb = new StringBuilder();
         String[] langs = {"Java","GO","Rust","Python","C#","TypeScript"};
         for(String lang : langs) {
@@ -12,5 +17,24 @@ public class StringOperations {
         }
         String result = sb.toString().trim();
         return result;
+    }
+    //join words
+    public static String joinStrings(String[] words){
+        StringBuilder sb= new StringBuilder();
+        for(String word : words){
+            sb.append(word).append(",");
+        }
+        return sb.toString();
+    }
+    //check palindromes
+    public static boolean checkIfPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
